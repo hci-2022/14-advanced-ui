1246eb5a47d3023fba3ed67eaf5373ca2849e489
Karl Stolley <karl.stolley@gmail.com>
2022-04-13 12:29:55 -0500

    Build out examples further for class

--- site.js
+++ site.js
@@ -1,4 +1,35 @@
-document.querySelector('html').className = 'js';
+// TODO: Uncomment this line when JavaScript-less styles have been written
+// document.querySelector('html').className = 'js';
+
+/*
+  Add-to-cart callbacks, logic, calculations, and events
+*/
+const store = document.querySelector('#store');
+
+store.addEventListener('click', handleAddToCartButtonClicks);
+
+function handleAddToCartButtonClicks(event) {
+  const button = event.target;
+  if (button.tagName !== 'BUTTON') return;
+}
+
+
+/*
+  Utility functions for handling money
+*/
+
+function lineItemSubtotal(wholeUnitPrice, quantity) {
+
+}
+
+function formatCurrency(wholeUnitValue, symbol) {
+
+}
+
+
+/*
+  Slideover behavior for store at mobile scales
+*/
 
 const cartButton = document.querySelector('a[href="#cart"]');
 
